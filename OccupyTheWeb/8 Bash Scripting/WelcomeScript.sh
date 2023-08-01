67e9e745b1f26935af6aed2b5457cea7d1b23ed4
papihoudini <ochiengcliff.co@gmail.com>
2023-08-01 05:03:02 -0400

    Adding functionality with variables and user input

---
+++ OccupyTheWeb/8 Bash Scripting/WelcomeScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompting user for input
+# Storing and displaying the user input
+
+echo "What's your name? "
+read name
+echo "How old are you? "
+read age
+echo "Hello $name, you are $age years old."
